9c23d08c48059abafd0b08f97602ae7a1e3c5948
sachinws <sachinsontakke111@gmail.com>
2022-09-30 20:13:18 +0530

    updated cart page as per naveen comment

--- src/basicJav/CartType.java
+++ src/basicJav/CartType.java
@@ -2,6 +2,12 @@ package basicJav;
 
 public class CartType {
 
+  private String name;
+  
+  public void afddcart() {
+	  System.out.println("sachinss");
+  }
+	
 	public static void main(String[] args) {
 
 		int i = 10;
