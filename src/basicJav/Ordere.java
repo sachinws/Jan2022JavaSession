e1ea5efceab648b4cfed120b3887435b28c36e41
sachinws <113722444+sachinws@users.noreply.github.com>
2022-10-01 01:04:15 +0530

    Create Ordere.java

---
+++ src/basicJav/Ordere.java
@@ -0,0 +1,19 @@
+package basicJav;
+
+public class Ordere {
+
+	
+	public void makeorder() {
+		System.out.println("order page ...");
+	}
+  
+  	public void morderid() {
+		System.out.println("order id ...");
+	}
+  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
