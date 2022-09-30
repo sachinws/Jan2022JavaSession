43fd0483f17a1e198d9d83eae4e1a2db700b7014
sachinws <sachinsontakke111@gmail.com>
2022-09-30 15:33:09 +0530

    newConstrutor added

---
+++ src/Constructor/CostructorCreate.java
@@ -0,0 +1,20 @@
+package Constructor;
+
+public class CostructorCreate {
+	
+	int a;
+	int b;
+	String name;
+	
+	public CostructorCreate() {
+		System.out.println("new Constructor created");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
