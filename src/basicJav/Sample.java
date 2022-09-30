d1ff03b0527aeab6879e789281c48e8974c61754
sachinws <113722444+sachinws@users.noreply.github.com>
2022-10-01 00:51:55 +0530

    Create Sample.java

---
+++ src/basicJav/Sample.java
@@ -0,0 +1,17 @@
+package basicJav;
+
+public class Sample {
+
+	public void login() {
+		String username = "naveen";
+    String pasword = "ram123";
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	
+
+	}
+
+}
